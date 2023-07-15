91792d80d235b16c63d02708889ccb42b27183c0
TAHIR ABBAS <aqeelshahzad1215@gmail.com>
2023-07-15 11:35:07 +0500

    fix ui of blog post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import { BlogCard } from '@/components'
 
 const Blog = () => {
   return (
-    <div className='min-h-screen  '>
+    <div className='min-h-screen mx-14 my-10 '>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   gap-4 ">
             <div className=" p-auto ">
                 <BlogCard data={{img : "/blog01.jpeg", name : "Founder Profile: Aditya Goenka" }} />
